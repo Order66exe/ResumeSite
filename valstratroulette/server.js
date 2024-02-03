178fbdc1e712b9af4be228137b9983d159d53c9b
Zane Slocum <zwslocum@yahoo.com>
2024-02-03 00:49:23 -0600

    Move server.js

--- valstratroulette/server.js
+++ valstratroulette/server.js
@@ -5,11 +5,11 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', async(req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.get('/:file', async(req, res) => {
-    res.sendFile(path.join(__dirname, req.params.file))
+    res.sendFile(path.join(__dirname, 'public/'+req.params.file))
 });
 
 app.listen(8080, () => {
